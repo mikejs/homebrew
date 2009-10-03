e2f5ecc08f649b6cce1c9d6a9a33c474748b8dea
Michael Stephens <me@mikej.st>
2009-10-02 22:09:26 -0400

    libgeoip Formula
    GeoIP is a C library that enables the user to find geographical
    and network information of an IP address.

---
+++ Library/Formula/libgeoip.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Libgeoip <Formula
+  url 'http://geolite.maxmind.com/download/geoip/api/c/GeoIP-1.4.6.tar.gz'
+  homepage 'http://www.maxmind.com/app/c'
+  md5 'cb14b1beeb40631a12676b11ca0c309a'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
